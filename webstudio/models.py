3ee904b9b7f0dbd9153475df5ffe8b4f0697be30
eclair-desertire <eclair.desertire@gmail.com>
2021-06-13 14:44:31 +0600

    Added Employee model

--- webstudio/models.py
+++ webstudio/models.py
@@ -9,5 +9,12 @@ class Order(models.Model):
     pass
 
 class Employee(models.Model):
-    pass
+    employee_photo=models.ImageField(null=True)
+    employee_fullname=models.CharField(max_length=300,default='')
+    employee_specialization=models.CharField(max_length=300,default='')
+    employee_email=models.EmailField(max_length=300,default='example@mail.com')
+    employee_info=models.TextField(null=True)
+    
+    def __str__(self):
+        return self.employee_fullname
 # Create your models here.
