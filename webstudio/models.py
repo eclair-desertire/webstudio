ec06e4fcf7844fe9a95b0df3cc2a66c423b59734
eclair-desertire <eclair.desertire@gmail.com>
2021-06-13 12:39:15 +0600

    Added application

--- webstudio/models.py
+++ webstudio/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
+class Works(models.Model):
+    pass
+
+class Order(models.Model):
+    pass
 # Create your models here.
--- webstudio/views.py
+++ webstudio/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def main_page(request):
+    return render(request,'webstudio/main_page.html',{})
 # Create your views here.
