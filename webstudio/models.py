e02f2303c5c23c2020d7873bc2d16982ea64e78e
eclair-desertire <eclair.desertire@gmail.com>
2021-06-21 19:07:32 +0600

    fff

--- webstudio/models.py
+++ webstudio/models.py
@@ -18,6 +18,11 @@ class Order(models.Model):
     order_title=models.CharField(max_length=200,null=True)
     order_info=models.TextField(null=True)
     order_contacts=models.CharField(max_length=300,null=True)
+    order_date=models.DateTimeField(null=True,blank=True)
+
+    def send(self):
+        self.order_date=timezone.now()
+        self.save()
 
     def __str__(self):
         return self.order_title
