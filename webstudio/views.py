444a0a510827fba474bb103997e8c8956d969ca5
Vladisalv2004 <63293377+Vladisalv2004@users.noreply.github.com>
2021-06-22 15:10:39 +0600

    Update views.py

--- webstudio/views.py
+++ webstudio/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from .forms import OrderForm
 
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+
+
 def main_page(request):
     return render(request,'webstudio/main_page.html',{})
 
 def order_new(request):
-    form=OrderForm()
+
+    if request.method == 'POST':
+        # если метод POST, проверим форму и отправим письмо
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            send_mail(f'{subject} от {from_email}', message,"вставь сюда нужны email", "сюда тот же самый  email")
+
+            return redirect('main_page')
+    else:
+        form = OrderForm()
     return render(request,'webstudio/order_new.html',{'form':form})
+
 # Create your views here.
