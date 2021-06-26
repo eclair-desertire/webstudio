bfadcb78b80aca6b518342574c9f6bb28de4dcbd
Vladisalv2004 <63293377+Vladisalv2004@users.noreply.github.com>
2021-06-26 19:50:29 +0600

    Update settings.py

--- knewit_webstudio/settings.py
+++ knewit_webstudio/settings.py
@@ -119,9 +119,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'knewit_webstudio/static'),
+]
+
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 
+EMAIL_HOST_USER = '' # email user
+EMAIL_HOST_PASSWORD = '!' # email password
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
