3ded24c092f450e9e1578458cf79db7235a93824
Vladisalv2004 <63293377+Vladisalv2004@users.noreply.github.com>
2021-06-27 12:33:00 +0600

    Update forms.py

--- webstudio/forms.py
+++ webstudio/forms.py
@@ -5,5 +5,15 @@ class OrderForm(forms.ModelForm):
     
     class Meta:
         model=Order
-        fields=('order_date','order_number','order_title','order_email','order_info')
-        
+        fields=('order_number','order_title','order_email','order_info','order_content')
+    
+        widgets = {
+                'order_title': forms.TextInput(attrs={'class': 'name'}),
+                'order_number': forms.TextInput(attrs={'id':'phone', 'class': 'number'}),
+                'order_email': forms.TextInput(attrs={'class': 'post_office'}),
+                'order_info': forms.TextInput(attrs={'class': 'what_to_develop'}),
+
+                'order_content': forms.Textarea(attrs={'class': 'textarea', 'minlength': '20', 'maxlength':'250', 'cols':'40', 'rows':'5', 'name':'massage'  }),
+
+            }
+            
